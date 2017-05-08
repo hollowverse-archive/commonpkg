645684e8309f857cb52364b49c5c6ed2db2b3d79
M.K. Safi <msafi+hollowverse@msafi.com>
2017-05-07 20:51:49 -0700

    This is alternative implementation that waits for the main intallation process to exit before doing commonpkg work

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
-import * as shelljs from 'shelljs'
+import * as shell from 'shelljs'
 import * as fs from 'fs'
-import * as path from 'path'
 import {dependenciesDiffer} from './dependenciesDiffer'
 
 // the following modules don't support ES6 module import, gotta use legacy import syntax
@@ -9,90 +8,81 @@ import pick = require('lodash.pick')
 import get = require('lodash.get')
 import defaults = require('lodash.defaultsdeep')
 
-// This process should start at `node_modules/commonpkg/`.
-// If we go one level up, we should be in `node_modules`
-shelljs.cd('..')
-
-if (
-  // Don't re-run the script if it was already run by `commonpkg` previously in the same process
-  process.env.commonpkgInstall === 'attempted' ||
-
-  // Also don't run the script if we're not inside `node_modules` at this point because that
-  // means this repo is running this script because we did `npm install` inside commonpkg repo
-  process.cwd().split(path.sep).pop() !== 'node_modules'
-) {
-  shelljs.exit(0)
+// Don't re-run the script if it was already run by `commonpkg` previously in the same process
+if (process.env.commonpkgInstall === 'attempted') {
+  shell.exit(0)
 }
 
-// If we go one more level up, we'll be at the desired location: the User Package that depends on
-// commonpkg.
-shelljs.cd('..')
+// We need to wait for the original install process to exit before beginning commonpkg's work
+// because we need all dependencies to be installed first.
+process.on('exit', () => {
+  // We need to find out our `pwd` first. It could be in one of two places
+  // 1. `node_modules/commonpkg`
+  // 2. `commonpkg/` (i.e. Me working on `commonpkg` and doing `npm install`)
+  // In the first case, we should go up two levels `../..` before proceeding.
+  // In the second case, we shouldn't change the PWD.
+  const isNmCommonpkg = fs.existsSync(`${process.cwd()}/../../node_modules/commonpkg/package.json`)
 
-const pwd = process.cwd()
-const userPackageJsonPath = `${pwd}/package.json`
-const userPackageJson = require(userPackageJsonPath)
+  if (isNmCommonpkg) {
+    shell.cd('../..')
+  }
 
-// Use Yarn if `yarn.lock` exists in the repo. Otherwise use npm.
-const packageManager = fs.existsSync(`${pwd}/yarn.lock`) ? 'yarn' : 'npm'
+  // Now that we are at the right location, we can start doing our thing
+  const pwd = process.cwd()
+  const userPackageJsonPath = `${pwd}/package.json`
+  const userPackageJson = require(userPackageJsonPath)
 
-// Get the name of the Common Package
-const theCommonPackageName = userPackageJson.commonpkg || userPackageJson.commonPackage
+  // Use Yarn if `yarn.lock` exists in the repo. Otherwise use npm.
+  const packageManager = fs.existsSync(`${pwd}/yarn.lock`) ? 'yarn' : 'npm'
 
-let theCommonPackagePackageJson
-let theCommonPackageExists = true
+  // Get the name of the Common Package
+  const theCommonPackageName = userPackageJson.commonpkg || userPackageJson.commonPackage
 
-// Check if theCommonPackage has been installed by the package manager
-try {
-  theCommonPackagePackageJson = require(`${theCommonPackageName}/package.json`)
-} catch (e) {
-  theCommonPackageExists = false
-}
+  if (!theCommonPackageName) {
+    console.log('commonpkg: could not find `commonpkg` field in your `package.json`. Exiting...')
+    shell.exit(0)
+  }
 
-if (!theCommonPackageExists) {
-  // If we're here, that means the theCommonPackage hasn't been installed yet.
-  // We need to install it before we can proceed.
+  // Check if theCommonPackage has been installed by the package manager
+  const theCommonPackageExists = fs.existsSync(`${pwd}/node_modules/${theCommonPackageName}/package.json`)
+  if (!theCommonPackageExists) {
+    console.log(
+      'commonpkg: your Common Package was not installed. Make sure it ' +
+      'is in your dependencies or devDependencies. Exiting...',
+    )
+    shell.exit(0)
+  }
 
-  const theCommonPackageVersion = (
-    get(userPackageJson, `devDependencies.${theCommonPackageName}`) ||
-    get(userPackageJson, `dependencies.${theCommonPackageName}`)
-  )
+  // Retrieve the content of the Common Package `package.json`
+  const theCommonPackagePackageJson = require(`${theCommonPackageName}/package.json`)
 
-  // At this point we have the data that we need about the Common Package. We can use
-  // the package manager of the repo to install it.
-  const installCommand = packageManager === 'yarn' ? 'add' : 'install'
+  // Let's check which parts of the Common Package are shared
+  const shareablePartsNames = theCommonPackagePackageJson.commonpkgShare
 
-  shelljs.exec(
-    `${packageManager} ${installCommand} ${theCommonPackageName}@${theCommonPackageVersion}`,
-  )
+  // If we couldn't find any shared parts, let's inform the consumer of the package.
+  if (!shareablePartsNames || !Array.isArray(shareablePartsNames)) {
+    console.log(
+      'commonpkg: the `package.json` of your Common Package is missing the `commonpkgShare` field. ' +
+      'Or the `commonpkgShare` field is not an array. Exiting...',
+    )
+    shell.exit(0)
+  }
 
-  // Now that we've installed the common package, let's try to grab its `package.json` again
-  theCommonPackagePackageJson = require(`${theCommonPackageName}/package.json`)
-}
+  // Get the shared parts as a JSON object.
+  const shareableProperties = pick(theCommonPackagePackageJson, shareablePartsNames)
 
-// At this point we hopefully have the Common Package. Let's check which parts of it are to be shared
-const shareablePartsNames = theCommonPackagePackageJson.commonpkgShare
+  // Merge the shared parts with the `package.json` of the User Package.
+  const userNewPackageJson = defaults({}, userPackageJson, shareableProperties)
 
-// If we couldn't find any shared parts, let's inform the consumer of the package.
-if (!Array.isArray(shareablePartsNames)) {
-  throw new Error(
-    'commonpkg: the `package.json` of your Common Package is missing the `commonpkgShare` field.',
-  )
-}
+  // Let's check if the dependencies of the two `package.json` files differ to determine
+  // whether we'll need to perform a new install or not.
+  const requiresNewInstall = dependenciesDiffer(userPackageJson, userNewPackageJson)
 
-// Get the shared parts as a JSON object.
-const shareableProperties = pick(theCommonPackagePackageJson, shareablePartsNames)
+  // Write the new `package.json` to the User Package.
+  fs.writeFileSync(userPackageJsonPath, `${JSON.stringify(userNewPackageJson, null, 2)}\n`)
 
-// Merge the shared parts with the `package.json` of the User Package.
-const userNewPackageJson = defaults({}, userPackageJson, shareableProperties)
-
-// Let's check if the dependencies of the two `package.json` files differ to determine
-// whether we'll need to perform a new install or not.
-const requiresNewInstall = dependenciesDiffer(userPackageJson, userNewPackageJson)
-
-// Write the new `package.json` to the User Package.
-fs.writeFileSync(userPackageJsonPath, `${JSON.stringify(userNewPackageJson, null, 2)}\n`)
-
-// Do another install with the new dependencies if necessary
-if (requiresNewInstall) {
-  shelljs.exec(`commonpkgInstall=attempted ${packageManager} install`)
-}
+  // Do another install with the new dependencies if necessary
+  if (requiresNewInstall) {
+    shell.exec(`commonpkgInstall=attempted ${packageManager} install`)
+  }
+})
